c15e6ab0743fd5ef28abec0cca43e76d830d00af
Ben Ebsworth <ben.ebsworth@gmail.com>
2019-11-22 22:55:56 -0800

    refactor file layout

--- talk/kube-summit-2019/presentation/slides/index.js
+++
@@ -1,8 +0,0 @@
-import { cloneElement } from 'react';
-import Intro from './01_intro';
-
-const slides = [].concat(
-  Intro,
-);
-
-export default slides.map((slide, i) => cloneElement(slide, { key: i }));
