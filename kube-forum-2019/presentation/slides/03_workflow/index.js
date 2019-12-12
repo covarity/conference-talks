056841b54ff728153ab6db954743fe9fa1a53487
Ben Ebsworth <ben.ebsworth@gmail.com>
2019-12-12 12:39:30 +1100

    added workflow back

--- kube-forum-2019/presentation/slides/03_workflow/index.js
+++ kube-forum-2019/presentation/slides/03_workflow/index.js
@@ -26,245 +26,7 @@ const COLOR_PALLETE = {
 };
 
 export default [
-  // <Slide
-  //   align="center flex-start"
-  //   transition={["zoom"]}
-  //   bgColor={COLOR_PALLETE.background}
-  // >
-  //   <Banner position="bottomRight" text={"@sycli"} />
-  //   <Banner position="bottomLeft" text={"@space_tj"} />
-  //   <Heading
-  //     margin="100px 0 100px 0"
-  //     size={5}
-  //     textColor={COLOR_PALLETE.textPrimary}
-  //     caps
-  //     lineHeight={1}
-  //   >
-  //     Workflow
-  //   </Heading>
-  //   <Layout style={{ alignItems: "center", height: 400 }}>
-  //     <Fill>
-  //       <Heading
-  //         size={4}
-  //         caps
-  //         textColor="secondary"
-  //         bgColor="white"
-  //         margin={10}
-  //       ></Heading>
-  //       <Text textColor={COLOR_PALLETE.textPrimary}>
-  //         A high level interface is provided to developer for managing policies
-  //         within their environments
-  //       </Text>
-  //     </Fill>
-  //     <Fill>
-  //       <Heading
-  //         size={4}
-  //         caps
-  //         textColor={COLOR_PALLETE.textPrimary}
-  //         margin={10}
-  //       >
-  //         values.yaml
-  //       </Heading>
-  //       <CodePane
-  //         textSize={25}
-  //         lang="yaml"
-  //         source={`
-  //       policies:
-  //         resourcesAllowed:
-  //           - name: dev-a
-  //             resources:
-  //             - Deployment
-  //             - ReplicaSet
-  //             - ConfigMap
-  //             - Secret
-  //         ingressBlock:
-  //         `}
-  //       />
-  //     </Fill>
-  //   </Layout>
-  // </Slide>,
-  // <Slide
-  //   align="center flex-start"
-  //   transition={["zoom"]}
-  //   bgColor={COLOR_PALLETE.background}
-  // >
-  //   <Banner position="bottomRight" text={"@sycli"} />
-  //   <Banner position="bottomLeft" text={"@space_tj"} />
-  //   <Heading
-  //     margin="100px 0 100px 0"
-  //     size={5}
-  //     textColor={COLOR_PALLETE.textPrimary}
-  //     caps
-  //     lineHeight={1}
-  //   >
-  //     Workflow
-  //   </Heading>
-  //   <Layout style={{ alignItems: "center", height: 400 }}>
-  //     <Fill>
-  //       <Heading
-  //         size={4}
-  //         caps
-  //         textColor="secondary"
-  //         bgColor="white"
-  //         margin={10}
-  //       ></Heading>
-  //       <Text textColor={COLOR_PALLETE.textPrimary}>
-  //         Tekton pipeline is triggered on PR to repository
-  //       </Text>
-  //     </Fill>
-  //     <Fill>
-  //       <Image src={Tekton} />
-  //     </Fill>
-  //   </Layout>
-  // </Slide>,
-  // <Slide
-  //   align="center flex-start"
-  //   transition={["zoom"]}
-  //   bgColor={COLOR_PALLETE.background}
-  // >
-  //   <Banner position="bottomRight" text={"@sycli"} />
-  //   <Banner position="bottomLeft" text={"@space_tj"} />
-  //   <Heading
-  //     margin="100px 0 100px 0"
-  //     size={5}
-  //     textColor={COLOR_PALLETE.textPrimary}
-  //     caps
-  //     lineHeight={1}
-  //   >
-  //     Workflow
-  //   </Heading>
-  //   <Layout style={{ alignItems: "center", height: 400 }}>
-  //     <Fill>
-  //       <Heading
-  //         size={4}
-  //         caps
-  //         textColor="secondary"
-  //         bgColor="white"
-  //         margin={10}
-  //       ></Heading>
-  //       <Text textColor={COLOR_PALLETE.textPrimary}>
-  //         Jinja2 templating engine generates OPA policy and corresponding unit
-  //         tests
-  //       </Text>
-  //     </Fill>
-  //     <Fill>
-  //       <Code bgColor={"#ffffff3d"} textColor={COLOR_PALLETE.textPrimary}>
-  //         jinja2 ./templates/*
-  //       </Code>
-  //     </Fill>
-  //   </Layout>
-  // </Slide>,
-  // <Slide
-  //   align="center flex-start"
-  //   transition={["zoom"]}
-  //   bgColor={COLOR_PALLETE.background}
-  // >
-  //   <Banner position="bottomRight" text={"@sycli"} />
-  //   <Banner position="bottomLeft" text={"@space_tj"} />
-  //   <Heading
-  //     margin="100px 0 100px 0"
-  //     size={5}
-  //     textColor={COLOR_PALLETE.textPrimary}
-  //     caps
-  //     lineHeight={1}
-  //   >
-  //     Workflow
-  //   </Heading>
-  //   <Layout style={{ alignItems: "center", height: 400 }}>
-  //     <Fill>
-  //       <Heading
-  //         size={4}
-  //         caps
-  //         textColor="secondary"
-  //         bgColor="white"
-  //         margin={10}
-  //       ></Heading>
-  //       <Text textColor={COLOR_PALLETE.textPrimary}>
-  //         Run unit tests using built-in opa testing capability
-  //       </Text>
-  //     </Fill>
-  //     <Fill>
-  //       <Code bgColor={"#ffffff3d"} textColor={COLOR_PALLETE.textPrimary}>
-  //         opa test ../output/tests/*
-  //       </Code>
-  //     </Fill>
-  //   </Layout>
-  // </Slide>,
-  // <Slide
-  //   align="center flex-start"
-  //   transition={["zoom"]}
-  //   bgColor={COLOR_PALLETE.background}
-  // >
-  //   <Banner position="bottomRight" text={"@sycli"} />
-  //   <Banner position="bottomLeft" text={"@space_tj"} />
-  //   <Heading
-  //     margin="100px 0 100px 0"
-  //     size={5}
-  //     textColor={COLOR_PALLETE.textPrimary}
-  //     caps
-  //     lineHeight={1}
-  //   >
-  //     Workflow
-  //   </Heading>
-  //   <Layout style={{ alignItems: "center", height: 400 }}>
-  //     <Fill>
-  //       <Heading
-  //         size={4}
-  //         caps
-  //         textColor="secondary"
-  //         bgColor="white"
-  //         margin={10}
-  //       ></Heading>
-  //       <Text textColor={COLOR_PALLETE.textPrimary}>
-  //         Run integration tests using anchorctl to assert expected behavior
-  //         within kubernetes
-  //       </Text>
-  //     </Fill>
-  //     <Fill>
-  //       <Code bgColor={"#ffffff3d"} textColor={COLOR_PALLETE.textPrimary}>
-  //         anchorctl run -f ./integration-tests/*{" "}
-  //       </Code>
-  //     </Fill>
-  //   </Layout>
-  // </Slide>,
-  // <Slide
-  //   align="center flex-start"
-  //   transition={["zoom"]}
-  //   bgColor={COLOR_PALLETE.background}
-  // >
-  //   <Banner position="bottomRight" text={"@sycli"} />
-  //   <Banner position="bottomLeft" text={"@space_tj"} />
-  //   <Heading
-  //     margin="100px 0 100px 0"
-  //     size={5}
-  //     textColor={COLOR_PALLETE.textPrimary}
-  //     caps
-  //     lineHeight={1}
-  //   >
-  //     Workflow
-  //   </Heading>
-  //   <Layout style={{ alignItems: "center", height: 400 }}>
-  //     <Fill>
-  //       <Heading
-  //         size={4}
-  //         caps
-  //         textColor="secondary"
-  //         bgColor="white"
-  //         margin={10}
-  //       ></Heading>
-  //       <Text textColor={COLOR_PALLETE.textPrimary}>
-  //         If all tests are successful then we commit back to the repository a
-  //         versioned snapshot of the OPA policy output and apply the changes to
-  //         the cluster
-  //       </Text>
-  //     </Fill>
-  //     <Fill>
-  //       <Code bgColor={"#ffffff3d"} textColor={COLOR_PALLETE.textPrimary}>
-  //         kubectl apply -f ./output/resources/*
-  //       </Code>
-  //     </Fill>
-  //   </Layout>
-  // </Slide>,
+  
   <Slide
     align="center center"
     transition={["zoom"]}
@@ -296,6 +58,245 @@ export default [
       CI Workflow
     </Heading>
     <Workflow labels={workflowLabels} />
+  </Slide>,
+  <Slide
+    align="center flex-start"
+    transition={["zoom"]}
+    bgColor={COLOR_PALLETE.background}
+  >
+    <Banner position="bottomRight" text={"@sycli"} />
+    <Banner position="bottomLeft" text={"@space_tj"} />
+    <Heading
+      margin="100px 0 100px 0"
+      size={5}
+      textColor={COLOR_PALLETE.textPrimary}
+      caps
+      lineHeight={1}
+    >
+      Workflow
+    </Heading>
+    <Layout style={{ alignItems: "center", height: 400 }}>
+      <Fill>
+        <Heading
+          size={4}
+          caps
+          textColor="secondary"
+          bgColor="white"
+          margin={10}
+        ></Heading>
+        <Text textColor={COLOR_PALLETE.textPrimary}>
+          A high level interface is provided to developer for managing policies
+          within their environments
+        </Text>
+      </Fill>
+      <Fill>
+        <Heading
+          size={4}
+          caps
+          textColor={COLOR_PALLETE.textPrimary}
+          margin={10}
+        >
+          values.yaml
+        </Heading>
+        <CodePane
+          textSize={25}
+          lang="yaml"
+          source={`
+        policies:
+          resourcesAllowed:
+            - name: dev-a
+              resources:
+              - Deployment
+              - ReplicaSet
+              - ConfigMap
+              - Secret
+          ingressBlock:
+          `}
+        />
+      </Fill>
+    </Layout>
+  </Slide>,
+  <Slide
+    align="center flex-start"
+    transition={["zoom"]}
+    bgColor={COLOR_PALLETE.background}
+  >
+    <Banner position="bottomRight" text={"@sycli"} />
+    <Banner position="bottomLeft" text={"@space_tj"} />
+    <Heading
+      margin="100px 0 100px 0"
+      size={5}
+      textColor={COLOR_PALLETE.textPrimary}
+      caps
+      lineHeight={1}
+    >
+      Workflow
+    </Heading>
+    <Layout style={{ alignItems: "center", height: 400 }}>
+      <Fill>
+        <Heading
+          size={4}
+          caps
+          textColor="secondary"
+          bgColor="white"
+          margin={10}
+        ></Heading>
+        <Text textColor={COLOR_PALLETE.textPrimary}>
+          Tekton pipeline is triggered on PR to repository
+        </Text>
+      </Fill>
+      <Fill>
+        <Image src={Tekton} />
+      </Fill>
+    </Layout>
+  </Slide>,
+  <Slide
+    align="center flex-start"
+    transition={["zoom"]}
+    bgColor={COLOR_PALLETE.background}
+  >
+    <Banner position="bottomRight" text={"@sycli"} />
+    <Banner position="bottomLeft" text={"@space_tj"} />
+    <Heading
+      margin="100px 0 100px 0"
+      size={5}
+      textColor={COLOR_PALLETE.textPrimary}
+      caps
+      lineHeight={1}
+    >
+      Workflow
+    </Heading>
+    <Layout style={{ alignItems: "center", height: 400 }}>
+      <Fill>
+        <Heading
+          size={4}
+          caps
+          textColor="secondary"
+          bgColor="white"
+          margin={10}
+        ></Heading>
+        <Text textColor={COLOR_PALLETE.textPrimary}>
+          Jinja2 templating engine generates OPA policy and corresponding unit
+          tests
+        </Text>
+      </Fill>
+      <Fill>
+        <Code bgColor={"#ffffff3d"} textColor={COLOR_PALLETE.textPrimary}>
+          jinja2 ./templates/*
+        </Code>
+      </Fill>
+    </Layout>
+  </Slide>,
+  <Slide
+    align="center flex-start"
+    transition={["zoom"]}
+    bgColor={COLOR_PALLETE.background}
+  >
+    <Banner position="bottomRight" text={"@sycli"} />
+    <Banner position="bottomLeft" text={"@space_tj"} />
+    <Heading
+      margin="100px 0 100px 0"
+      size={5}
+      textColor={COLOR_PALLETE.textPrimary}
+      caps
+      lineHeight={1}
+    >
+      Workflow
+    </Heading>
+    <Layout style={{ alignItems: "center", height: 400 }}>
+      <Fill>
+        <Heading
+          size={4}
+          caps
+          textColor="secondary"
+          bgColor="white"
+          margin={10}
+        ></Heading>
+        <Text textColor={COLOR_PALLETE.textPrimary}>
+          Run unit tests using built-in opa testing capability
+        </Text>
+      </Fill>
+      <Fill>
+        <Code bgColor={"#ffffff3d"} textColor={COLOR_PALLETE.textPrimary}>
+          opa test ../output/tests/*
+        </Code>
+      </Fill>
+    </Layout>
+  </Slide>,
+  <Slide
+    align="center flex-start"
+    transition={["zoom"]}
+    bgColor={COLOR_PALLETE.background}
+  >
+    <Banner position="bottomRight" text={"@sycli"} />
+    <Banner position="bottomLeft" text={"@space_tj"} />
+    <Heading
+      margin="100px 0 100px 0"
+      size={5}
+      textColor={COLOR_PALLETE.textPrimary}
+      caps
+      lineHeight={1}
+    >
+      Workflow
+    </Heading>
+    <Layout style={{ alignItems: "center", height: 400 }}>
+      <Fill>
+        <Heading
+          size={4}
+          caps
+          textColor="secondary"
+          bgColor="white"
+          margin={10}
+        ></Heading>
+        <Text textColor={COLOR_PALLETE.textPrimary}>
+          Run integration tests using anchorctl to assert expected behavior
+          within kubernetes
+        </Text>
+      </Fill>
+      <Fill>
+        <Code bgColor={"#ffffff3d"} textColor={COLOR_PALLETE.textPrimary}>
+          anchorctl run -f ./integration-tests/*{" "}
+        </Code>
+      </Fill>
+    </Layout>
+  </Slide>,
+  <Slide
+    align="center flex-start"
+    transition={["zoom"]}
+    bgColor={COLOR_PALLETE.background}
+  >
+    <Banner position="bottomRight" text={"@sycli"} />
+    <Banner position="bottomLeft" text={"@space_tj"} />
+    <Heading
+      margin="100px 0 100px 0"
+      size={5}
+      textColor={COLOR_PALLETE.textPrimary}
+      caps
+      lineHeight={1}
+    >
+      Workflow
+    </Heading>
+    <Layout style={{ alignItems: "center", height: 400 }}>
+      <Fill>
+        <Heading
+          size={4}
+          caps
+          textColor="secondary"
+          bgColor="white"
+          margin={10}
+        ></Heading>
+        <Text textColor={COLOR_PALLETE.textPrimary}>
+          If all tests are successful then we commit back to the repository a
+          versioned snapshot of the OPA policy output and apply the changes to
+          the cluster
+        </Text>
+      </Fill>
+      <Fill>
+        <Code bgColor={"#ffffff3d"} textColor={COLOR_PALLETE.textPrimary}>
+          kubectl apply -f ./output/resources/*
+        </Code>
+      </Fill>
+    </Layout>
   </Slide>,
   <Slide
     align="center center"
@@ -340,6 +341,19 @@ export default [
       <Image margin="0" height={120} src={GitHub} />
       <Text margin="0 0 0 40px" textColor={COLOR_PALLETE.textPrimary}>
         https://github.com/covarity/anchorctl
+      </Text>
+    </div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 30,
+      }}
+    >
+      <Image margin="0" height={120} src={GitHub} />
+      <Text margin="0 0 0 40px" textColor={COLOR_PALLETE.textPrimary}>
+        https://github.com/covarity/examples
       </Text>
     </div>
   </Slide>,
